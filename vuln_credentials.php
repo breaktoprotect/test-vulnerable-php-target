1c28d8bcd8d51e39ef0f15903acdd397e76b06de
JS @breaktoprotect <breaktoprotect@gmail.com>
2023-02-18 22:31:03 +0800

    Create vuln_credentials.php

---
+++ vuln_credentials.php
@@ -0,0 +1,12 @@
+<?php
+$username = "user123";
+$password = "password123";
+$passwd = "password456";
+$user = "user789";
+$apikey = "api123";
+$api_key = "api456";
+$token = "token123";
+$access_token = "token456";
+$secret = "secret123";
+$private_key = "key123";
+?>
