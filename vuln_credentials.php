ef72c49fc5018f899b18e9ec044f5a51ace0ab5a
JS @breaktoprotect <breaktoprotect@gmail.com>
2023-02-18 22:41:50 +0800

    Update vuln_credentials.php

--- vuln_credentials.php
+++ vuln_credentials.php
@@ -1,4 +1,8 @@
 <?php
+$jwt_access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+
+
+
 $username = "user123";
 $password = "password123";
 $passwd = "password456";
